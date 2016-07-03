6b1a85d96654c46fe11003fe98b52c5ada369c03
YetaWF <mikevdm@YetaWF.com>
2016-07-03 07:06:56 -0400

    Megamenu support in main page menu (using SubModule), attach a module to a menu action (bootstrap menu only) make bootstrap menu a regular skin module (eliminates awkward skin evaluation of menu) and allows editing module through module menu

--- BootstrapSample/Properties/AssemblyInfo.cs
+++ BootstrapSample/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
 
 [assembly: RequiresAddOnGlobal("getbootstrap.com", "bootstrap-less")]
 
