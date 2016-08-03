e68745ebbe3165c0de7919cef4e46649dc1c538a
YetaWF <mikevdm@YetaWF.com>
2016-08-03 19:45:06 -0400

    add support for standard Bootstrap skin and customizable Bootstrap skins (via config.json)

---
+++ BootstrapSample/Addons/_Skins/BootstrapSkin/fixup.js
@@ -0,0 +1,10 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+// This assumes that bootstrap.js is loaded before jquery-ui
+// bootstrap and jquery both use button() so we rename bootstrap's to bootstrapButton
+$.fn.bootstrapButton = $.fn.button     // save the bootstrap function
+$.fn.button = $.fn.button.noConflict();// return $.fn.button to previously assigned value
+
+// bootstrap and jquery both use tooltip() so we rename bootstrap's to bootstrapTooltip
+$.fn.bootstrapTooltip = $.fn.tooltip    // save the bootstrap function
+$.fn.tooltip = $.fn.tooltip.noConflict() // return $.fn.tooltip to previously assigned value
