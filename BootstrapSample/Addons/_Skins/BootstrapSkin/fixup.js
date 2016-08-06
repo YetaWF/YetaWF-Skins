1d26fd0128b1aeea93e838a2db0c29d8b8cd0d40
YetaWF <mikevdm@YetaWF.com>
2016-08-06 10:17:10 -0400

    "last" support for filelistJS.txt to add some files after all addons have been added this was required to insure that scripts depending on multiple addons run after all addons are available (notably to fix jquery-ui/bootstrap conflicts)

--- BootstrapSample/Addons/_Skins/BootstrapSkin/fixup.js
+++ BootstrapSample/Addons/_Skins/BootstrapSkin/fixup.js
@@ -1,6 +1,10 @@
 ﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
 
-// This assumes that bootstrap.js is loaded before jquery-ui
+// This assumes that the code in jquery-ui's fixup.js runs later
+
+/* this is used to verify that the Bootstrap fixup is used */
+var Y_YetaWFBootstrap_Fixup = true;
+
 // bootstrap and jquery both use button() so we rename bootstrap's to bootstrapButton
 $.fn.bootstrapButton = $.fn.button     // save the bootstrap function
 $.fn.button = $.fn.button.noConflict();// return $.fn.button to previously assigned value
