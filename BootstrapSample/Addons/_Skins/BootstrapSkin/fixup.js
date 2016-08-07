d3da0c1594df585fae37708f1bffbdd341b18d03
YetaWF <mikevdm@YetaWF.com>
2016-08-07 16:24:45 -0400

    split jquery-ui into jquery-ui and jquery-ui-themes addons fix how bootstrap/jqueryui conflict is resolved

--- BootstrapSample/Addons/_Skins/BootstrapSkin/fixup.js
+++ BootstrapSample/Addons/_Skins/BootstrapSkin/fixup.js
@@ -1,14 +1,12 @@
 ﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
 
-// This assumes that the code in jquery-ui's fixup.js runs later
+// We're saving some bootstrap functions that collide with jquery-ui
 
 /* this is used to verify that the Bootstrap fixup is used */
 var Y_YetaWFBootstrap_Fixup = true;
 
 // bootstrap and jquery both use button() so we rename bootstrap's to bootstrapButton
 $.fn.bootstrapButton = $.fn.button     // save the bootstrap function
-$.fn.button = $.fn.button.noConflict();// return $.fn.button to previously assigned value
 
 // bootstrap and jquery both use tooltip() so we rename bootstrap's to bootstrapTooltip
 $.fn.bootstrapTooltip = $.fn.tooltip    // save the bootstrap function
-$.fn.tooltip = $.fn.tooltip.noConflict() // return $.fn.tooltip to previously assigned value
