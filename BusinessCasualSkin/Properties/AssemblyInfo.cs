d5992a3cc2ad666396066639deb572a5178834ba
YetaWF <mikevdm@YetaWF.com>
2016-09-27 21:33:42 -0400

    Simple sample bootstrap skin - ported BusinessCasual skin as seen on https://startbootstrap.com/template-categories/all/

---
+++ BusinessCasualSkin/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using YetaWF.Core.Packages;
+using YetaWF.PackageAttributes;
+
+[assembly: AssemblyTitle("BusinessCasualSkin")]
+[assembly: AssemblyDescription("BusinessCasual skin as seen on https://startbootstrap.com/template-categories/all/")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Softel vdm, Inc.")]
+[assembly: AssemblyProduct("BusinessCasualSkin")]
+[assembly: AssemblyCopyright("Copyright © 2016 - Softel vdm, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.7.0")]
+
+[assembly: RequiresAddOnGlobal("getbootstrap.com", "bootstrap-less")]
+
+[assembly: Package(PackageTypeEnum.Skin, "YetaWF")]
+[assembly: PackageInfo("http://YetaWF.com/UpdateServer",
+    "http://yetawf.com/Documentation/YetaWF/BusinessCasualSkin",
+    "http://yetawf.com/Documentation/YetaWF/BusinessCasualSkin/Support",
+    "http://yetawf.com/Documentation/YetaWF/BusinessCasualSkin#Release%20Notice",
+    "http://yetawf.com/Documentation/YetaWF/BusinessCasualSkin#License")]
---
+++ BusinessCasualSkin/Startup/Info.cs
@@ -0,0 +1,11 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+using YetaWF.Core.Addons;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Modules.BusinessCasualSkin.Addons {
+
+    public class Info : IAddOnSupport {
+        public void AddSupport(YetaWFManager manager) { }
+    }
+}
