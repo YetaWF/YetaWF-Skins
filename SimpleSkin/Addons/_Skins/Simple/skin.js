538f6a87f5b4012eacb375a0e3ffc2967a84a7f5
YetaWF <mikevdm@YetaWF.com>
2017-07-01 21:27:45 -0400

    remove unused skins

--- SimpleSkin/Addons/_Skins/Simple/skin.js
+++
@@ -1,3 +0,0 @@
-﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
-/* Skin */
-
--- YetaWFSkin/Addons/_Skins/YetaWFSkin/Skin.js
+++
@@ -1,6 +0,0 @@
-﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
-
-/* YetaWFSkin Skin */
-/* YetaWFSkin Skin */
-/* YetaWFSkin Skin */
-
