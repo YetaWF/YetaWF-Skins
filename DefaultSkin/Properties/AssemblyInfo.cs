d398b245ed9e83c6757785705a988d915db17448
YetaWF <mikevdm@YetaWF.com>
2017-12-02 10:39:51 -0500

    new default skin with support for bootstrap themes (replaces bootstrapsample)

---
+++ DefaultSkin/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using YetaWF.Core.Packages;
+using YetaWF.PackageAttributes;
+
+[assembly: AssemblyTitle("DefaultSkin")]
+[assembly: AssemblyDescription("DefaultSkin for YetaWF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Softel vdm, Inc.")]
+[assembly: AssemblyProduct("DefaultSkin")]
+[assembly: AssemblyCopyright("Copyright © 2017 - Softel vdm, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("2.7.0.0")]
+
+[assembly: Package(PackageTypeEnum.Module, "YetaWF")]
+[assembly: PackageInfo("http://softelvdm.com/UpdateServer",
+    "http://softelvdm.com/Documentation/YetaWF/DefaultSkin",
+    "http://softelvdm.com/Documentation/YetaWF/Support",
+    "http://softelvdm.com/Documentation/YetaWF/DefaultSkin#Release%20Notice",
+    "http://softelvdm.com/Documentation/YetaWF/DefaultSkin#License")]
---
+++ DefaultSkin/Startup/Info.cs
@@ -0,0 +1,11 @@
+/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using YetaWF.Core.Addons;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Modules.DefaultSkin.Addons {
+
+    public class Info : IAddOnSupport {
+        public void AddSupport(YetaWFManager manager) { }
+    }
+}
