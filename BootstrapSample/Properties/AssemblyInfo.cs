2ffd80adf5e022db59ed6f9dc14c15f17d7d09e9
YetaWF <mikevdm@YetaWF.com>
2018-04-14 20:33:57 -0400

    prepare for 3.0.0

--- BootstrapSample/Properties/AssemblyInfo.cs
+++ BootstrapSample/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.8.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
 
 [assembly: Package(PackageTypeEnum.Skin, "YetaWF")]
 [assembly: PackageInfo("https://YetaWF.com/UpdateServer",
