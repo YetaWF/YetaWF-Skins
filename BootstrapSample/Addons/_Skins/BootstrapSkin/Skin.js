2c7e1eaeebd7c29e28beb06ed63f035f11b0c3c7
YetaWF <mikevdm@YetaWF.com>
2018-05-06 16:15:13 -0400

    obsolete, removed for bootstrap 4

--- BootstrapSample/Addons/_Skins/BootstrapSkin/Skin.js
+++
@@ -1,6 +0,0 @@
-/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
-
-/* BootstrapSample Skin */
-/* BootstrapSample Skin */
-/* BootstrapSample Skin */
-
--- BusinessCasualSkin/Addons/_Skins/BusinessCasual/Skin.js
+++
@@ -1,3 +0,0 @@
-/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
-
-/* BusinessCasual Skin */
