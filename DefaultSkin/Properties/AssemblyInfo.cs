d56ecb2703ff8f295ecdcdfd9aa0aba9d43c6af7
YetaWF <mikevdm@YetaWF.com>
2019-01-02 17:53:14 -0500

    2019

--- DefaultSkin/Properties/AssemblyInfo.cs
+++ DefaultSkin/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -10,7 +10,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softel vdm, Inc.")]
 [assembly: AssemblyProduct("DefaultSkin")]
-[assembly: AssemblyCopyright("Copyright © 2018 - Softel vdm, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Softel vdm, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- DefaultSkin/Startup/Info.cs
+++ DefaultSkin/Startup/Info.cs
@@ -1,4 +1,4 @@
-/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using System.Threading.Tasks;
 using YetaWF.Core.Addons;
