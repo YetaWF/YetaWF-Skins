8421f0b575d69bae3ec30142789fa31c1e4228d9
YetaWF <mikevdm@YetaWF.com>
2019-02-16 20:53:33 -0500

    prepare for 4.2

--- DefaultSkin/Properties/AssemblyInfo.cs
+++ DefaultSkin/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.2.0.0")]
 
 [assembly: Package(PackageTypeEnum.Skin, "YetaWF")]
 [assembly: PackageInfo("https://YetaWF.com/UpdateServer",
