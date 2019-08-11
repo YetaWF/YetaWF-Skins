b6632c47f4555e3a490cbaef0bf543a4f56929b6
YetaWF <mikevdm@YetaWF.com>
2019-08-11 18:15:06 -0400

    nginx skin

---
+++ NginxSkin/Pages/HTML/Plain.cs
@@ -0,0 +1,20 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using YetaWF.Core.Components;
+using YetaWF.Core.Packages;
+using YetaWF.Skins.NginxSkin.Controllers;
+
+namespace YetaWF.Skins.NginxSkin.Pages {
+
+    /// <summary>
+    /// Implements the Plain page.
+    /// </summary>
+    public class PlainPage : PlainPageBase {
+
+        /// <summary>
+        /// Returns the package implementing the page.
+        /// </summary>
+        /// <returns>Returns the package implementing the page.</returns>
+        public override Package GetPackage() { return AreaRegistration.CurrentPackage; }
+    }
+}
---
+++ NginxSkin/Pages/HTML/Popup.cs
@@ -0,0 +1,54 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using YetaWF.Core.Components;
+using YetaWF.Core.Packages;
+using YetaWF.Skins.NginxSkin.Controllers;
+
+namespace YetaWF.Skins.NginxSkin.Pages {
+
+    /// <summary>
+    /// Implements the Popup page.
+    /// </summary>
+    public class PopupPage : PopupPageBase {
+        /// <summary>
+        /// Returns the package implementing the page.
+        /// </summary>
+        /// <returns>Returns the package implementing the page.</returns>
+        public override Package GetPackage() { return AreaRegistration.CurrentPackage; }
+        /// <summary>
+        /// Returns the name of the page.
+        /// </summary>
+        /// <returns>Returns the name of the page.</returns>
+        public override string GetPageName() { return "Popup"; }
+    }
+    /// <summary>
+    /// Implements the PopupSmall page.
+    /// </summary>
+    public class PopupSmallPage : PopupPageBase {
+        /// <summary>
+        /// Returns the package implementing the page.
+        /// </summary>
+        /// <returns>Returns the package implementing the page.</returns>
+        public override Package GetPackage() { return AreaRegistration.CurrentPackage; }
+        /// <summary>
+        /// Returns the name of the page.
+        /// </summary>
+        /// <returns>Returns the name of the page.</returns>
+        public override string GetPageName() { return "PopupSmall"; }
+    }
+    /// <summary>
+    /// Implements the PopupMedium page.
+    /// </summary>
+    public class PopupMediumPage : PopupPageBase {
+        /// <summary>
+        /// Returns the package implementing the page.
+        /// </summary>
+        /// <returns>Returns the package implementing the page.</returns>
+        public override Package GetPackage() { return AreaRegistration.CurrentPackage; }
+        /// <summary>
+        /// Returns the name of the page.
+        /// </summary>
+        /// <returns>Returns the name of the page.</returns>
+        public override string GetPageName() { return "PopupMedium"; }
+    }
+}
