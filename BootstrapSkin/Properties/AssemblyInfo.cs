e1dff6b37cdb65ec6819cc62599eab97a305d2be
Mike <mikevdm@mikevdm.com>
2019-12-23 12:07:56 -0500

    5.1.0, prepare 2020

--- BootstrapSkin/Properties/AssemblyInfo.cs
+++ BootstrapSkin/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+/* Copyright © 2020 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -10,12 +10,12 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Softel vdm, Inc.")]
 [assembly: AssemblyProduct("BootstrapSkin")]
-[assembly: AssemblyCopyright("Copyright © 2019 - Softel vdm, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2020 - Softel vdm, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.1.0.0")]
 
 [assembly: Package(PackageTypeEnum.Skin, "YetaWF")]
 [assembly: PackageInfo("https://YetaWF.com/UpdateServer",
--- DefaultSkin/Startup/AreaRegistration.cs
+++ DefaultSkin/Startup/AreaRegistration.cs
@@ -1,4 +1,4 @@
-/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+/* Copyright ï¿½2020 Softel vdm, Inc.. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using YetaWF.Core.Packages;
 
